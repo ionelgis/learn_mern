8b0b4a589e5fbdac00aa6d3566128f20e9f0b1bb
Ionel <ionegis@gmail.com>
2018-06-13 00:02:37 +0300

    add login feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
+
 import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
@@ -10,6 +14,17 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
 import "./App.css";
+
+//check for token
+
+if (localStorage.jwtToken) {
+  //set auth token header aut
+  setAuthToken(localStorage.jwtToken);
+  //decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // se user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
