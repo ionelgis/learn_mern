4c6cbfc0ffa17c338fc80af68d323ae888c9cc58
Ionel <ionegis@gmail.com>
2018-06-14 01:32:55 +0300

    add profile actions

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 
 import store from "./store";
 
@@ -12,6 +13,7 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Dashboard from "./components/dashboard/Dashboard";
 
 import "./App.css";
 
@@ -29,7 +31,8 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     //logout the user
     store.dispatch(logoutUser);
-    //TODO clear current profile
+    // clear current profile
+    store.dispatch(clearCurrentProfile);
 
     //redirect to login
     window.location.href = "/login";
@@ -48,6 +51,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
 
             <Footer />
