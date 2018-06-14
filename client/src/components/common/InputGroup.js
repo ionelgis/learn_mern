6a5d820007436f038a73d6b3809abba389ed2aa1
Ionel <ionegis@gmail.com>
2018-06-15 01:37:57 +0300

    add create profile react component

--- client/src/components/common/InputGroup.js
+++ client/src/components/common/InputGroup.js
@@ -13,8 +13,8 @@ const InputGroup = ({
 }) => {
   return (
     <div className="input-group mb-3">
-      <div className="input group prepend">
-        <span className="input group text">
+      <div className="input-group-prepend">
+        <span className="input-group-text">
           <i className={icon} />
         </span>
       </div>
