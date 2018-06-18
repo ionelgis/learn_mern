d42d4ed339678ce0ec562414ebfae2629b65a9f1
Ionel <ionegis@gmail.com>
2018-06-18 22:17:13 +0300

    add add-experience component and functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -31,6 +31,19 @@ export const createProfile = (profileData, history) => dispatch => {
     );
 };
 
+//add experience action
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post("/api/profile/experience", expData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 //delete account and profile action
 export const deleteAccount = () => dispatch => {
   if (window.confirm("Are you sure? This can NOT be undone")) {
